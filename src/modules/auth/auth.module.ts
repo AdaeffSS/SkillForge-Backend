31950175895a5b4e8ad394d7e32bc13ca01e24bb
Stepa Adaeff <contact@adaeff.ru>
2025-06-18 23:55:33 +0300

    feat: realized generate access and refresh tokens

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -4,9 +4,14 @@ import { AuthController } from "./auth.controller";
 import { ZvonokModule } from "../zvonok/zvonok.module";
 import { SequelizeModule } from "@nestjs/sequelize";
 import { Otp } from "./entites/otp.entity";
+import { JwtModule } from "@nestjs/jwt";
 
 @Module({
-  imports: [ZvonokModule, SequelizeModule.forFeature([Otp])],
+  imports: [
+    JwtModule.register({}),
+    ZvonokModule,
+    SequelizeModule.forFeature([Otp]),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -2,14 +2,31 @@ import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { ZvonokService } from "../zvonok/zvonok.service";
 import { InjectModel } from "@nestjs/sequelize";
 import { Otp } from "./entites/otp.entity";
-import { HttpErrorByCode } from "@nestjs/common/utils/http-error-by-code.util";
+import { JwtService } from "@nestjs/jwt";
+import * as process from "node:process";
 
 @Injectable()
 export class AuthService {
   constructor(
     private readonly zvonokService: ZvonokService,
     @InjectModel(Otp) private readonly otpModel: typeof Otp,
+    private readonly jwtService: JwtService,
   ) {}
+
+  async generateTokens(payload: { sub: string }) {
+    const [accessToken, refreshToken] = await Promise.all([
+      this.jwtService.signAsync(payload, {
+        secret: process.env.JWT_ACCESS_SECRET,
+        expiresIn: process.env.JWT_ACCESS_EXPIRATION,
+      }),
+      this.jwtService.signAsync(payload, {
+        secret: process.env.JWT_REFRESH_SECRET,
+        expiresIn: process.env.JWT_REFRESH_EXPIRATION,
+      }),
+    ]);
+
+    return { accessToken, refreshToken };
+  }
 
   async requestCode(phoneNumber: string) {
     const code = await this.zvonokService.sendCall(phoneNumber);
@@ -25,7 +42,6 @@ export class AuthService {
   }
 
   async verifyCode(phoneNumber: string, code: string) {
-
     if (!phoneNumber || !code) {
       console.log(phoneNumber, code);
       throw new HttpException("Неверный запрос", 400);
@@ -34,19 +50,20 @@ export class AuthService {
     const otp = await this.otpModel.findOne({
       where: { phoneNumber, code },
       order: [["createdAt", "DESC"]],
-    })
+    });
 
     if (!otp) {
-      throw new HttpException("Неверный код подтверждения", HttpStatus.UNAUTHORIZED);
+      throw new HttpException(
+        "Неверный код подтверждения",
+        HttpStatus.UNAUTHORIZED,
+      );
     }
 
     if (otp.expiresAt < new Date()) {
-      throw new HttpException("Код подтверждения истек",  HttpStatus.GONE);
+      throw new HttpException("Код подтверждения истек", HttpStatus.GONE);
     }
     await otp.destroy();
 
-    return { message: "Код подтвержден" };
-
+    return await this.generateTokens({ sub: phoneNumber });
   }
-
 }
